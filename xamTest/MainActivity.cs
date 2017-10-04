0f3dfb93d4fb508fd985861f53f5ed540fb62c54
hbeckmann <hunterbeckmann@gmail.com>
2017-10-04 00:25:28 -0600

    Add project files.

---
+++ xamTest/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace xamTest
+{
+    [Activity(Label = "xamTest", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            Button button = FindViewById<Button>(Resource.Id.lifeCounterMenuButton);
+
+
+
+        }
+    }
+}
+
