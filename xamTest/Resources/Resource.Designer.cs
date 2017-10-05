39f4c93eb06b9002c1053b66ec02b8c87e62fd4a
hbeckmann <hunterbeckmann@gmail.com>
2017-10-04 23:27:17 -0600

    button changes in life counter

--- xamTest/Resources/Resource.Designer.cs
+++ xamTest/Resources/Resource.Designer.cs
@@ -41,26 +41,45 @@ namespace xamTest
 			}
 		}
 		
+		public partial class Drawable
+		{
+			
+			// aapt resource value: 0x7f020000
+			public const int round_button = 2130837504;
+			
+			static Drawable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Drawable()
+			{
+			}
+		}
+		
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f040003
-			public const int lifeCounterLayout = 2130968579;
+			// aapt resource value: 0x7f050000
+			public const int button1 = 2131034112;
 			
-			// aapt resource value: 0x7f040004
-			public const int lifeCounterMenuButton = 2130968580;
+			// aapt resource value: 0x7f050004
+			public const int lifeCounterLayout = 2131034116;
 			
-			// aapt resource value: 0x7f040001
-			public const int linearLayout1 = 2130968577;
+			// aapt resource value: 0x7f050005
+			public const int lifeCounterMenuButton = 2131034117;
 			
-			// aapt resource value: 0x7f040005
-			public const int menuViewPager = 2130968581;
+			// aapt resource value: 0x7f050001
+			public const int linearLayout1 = 2131034113;
 			
-			// aapt resource value: 0x7f040002
-			public const int space2 = 2130968578;
+			// aapt resource value: 0x7f050006
+			public const int menuViewPager = 2131034118;
 			
-			// aapt resource value: 0x7f040000
-			public const int textView1 = 2130968576;
+			// aapt resource value: 0x7f050002
+			public const int space2 = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int textView1 = 2131034115;
 			
 			static Id()
 			{
@@ -75,14 +94,14 @@ namespace xamTest
 		public partial class Layout
 		{
 			
-			// aapt resource value: 0x7f020000
-			public const int LifeCounter = 2130837504;
+			// aapt resource value: 0x7f030000
+			public const int LifeCounter = 2130903040;
 			
-			// aapt resource value: 0x7f020001
-			public const int Main = 2130837505;
+			// aapt resource value: 0x7f030001
+			public const int Main = 2130903041;
 			
-			// aapt resource value: 0x7f020002
-			public const int Menu = 2130837506;
+			// aapt resource value: 0x7f030002
+			public const int Menu = 2130903042;
 			
 			static Layout()
 			{
@@ -97,8 +116,8 @@ namespace xamTest
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int app_name = 2130903040;
+			// aapt resource value: 0x7f040000
+			public const int app_name = 2130968576;
 			
 			static String()
 			{
