5faac685acbad7aeb0c54b07f5e32fa01c9af3a1
hbeckmann <hunterbeckmann@gmail.com>
2017-10-07 03:37:52 -0600

    Timer functionality start

--- xamTest/LifeCounterFragment.cs
+++ xamTest/LifeCounterFragment.cs
@@ -2,6 +2,8 @@
 using Android.Widget;
 using Android.OS;
 using Android.Views;
+using System.Threading;
+using System;
 
 namespace xamTest
 {
@@ -22,6 +24,9 @@ namespace xamTest
             Button decreaseButton = View.FindViewById<Button>(Resource.Id.topDecreaseButton);
             Button increaseButton = View.FindViewById<Button>(Resource.Id.topIncreaseButton);
             TextView topLifeCounter = View.FindViewById<TextView>(Resource.Id.topLifeCounter);
+            TextView gameTimer = View.FindViewById<TextView>(Resource.Id.gameTimer);
+            StartTimer(1000, gameTimer);
+
             decreaseButton.Click += delegate
             {
                 if (int.Parse(topLifeCounter.Text) > 0)
@@ -37,6 +42,35 @@ namespace xamTest
 
         }
 
+        public void StartTimer(int time, TextView timerGUI)
+        {
+            TimeInfo timeInfo = new TimeInfo(timerGUI);
+            Timer t = new Timer(new TimerCallback(TimerTick), timeInfo, time, time);
+        }
+
+        public void TimerTick (object state)
+        {
+            TimeInfo t = (TimeInfo)state;
+            Activity.RunOnUiThread(() => t.timerGUI.Text = (DateTime.Now.Second - t.startTime.Second).ToString());
+            Console.WriteLine("TICK!");
+           
+        }
+
     }
+
+
+    public class TimeInfo 
+    {
+        public TextView timerGUI;
+
+        public TimeInfo(TextView timerGUI)
+        {
+            this.timerGUI = timerGUI;
+        }
+
+        public DateTime startTime = DateTime.Now;
+    }
+
+
 }
 
--- xamTest/Resources/Resource.Designer.cs
+++ xamTest/Resources/Resource.Designer.cs
@@ -45,7 +45,10 @@ namespace xamTest
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int round_button = 2130837504;
+			public const int icrefreshblack24dp = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int round_button = 2130837505;
 			
 			static Drawable()
 			{
@@ -60,17 +63,26 @@ namespace xamTest
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050007
+			public const int gameTimer = 2131034119;
+			
 			// aapt resource value: 0x7f050008
-			public const int lifeCounterLayout = 2131034120;
+			public const int lifeCounterIcons = 2131034120;
 			
-			// aapt resource value: 0x7f050009
-			public const int lifeCounterMenuButton = 2131034121;
+			// aapt resource value: 0x7f05000d
+			public const int lifeCounterLayout = 2131034125;
 			
-			// aapt resource value: 0x7f050005
-			public const int linearLayout1 = 2131034117;
+			// aapt resource value: 0x7f05000e
+			public const int lifeCounterMenuButton = 2131034126;
 			
 			// aapt resource value: 0x7f05000a
-			public const int menuViewPager = 2131034122;
+			public const int linearLayout1 = 2131034122;
+			
+			// aapt resource value: 0x7f05000f
+			public const int menuViewPager = 2131034127;
+			
+			// aapt resource value: 0x7f050009
+			public const int refreshTimer = 2131034121;
 			
 			// aapt resource value: 0x7f050001
 			public const int relativeLayout2 = 2131034113;
@@ -78,11 +90,17 @@ namespace xamTest
 			// aapt resource value: 0x7f050000
 			public const int relativeLayout3 = 2131034112;
 			
-			// aapt resource value: 0x7f050006
-			public const int space2 = 2131034118;
+			// aapt resource value: 0x7f050005
+			public const int relativeLayout4 = 2131034117;
 			
-			// aapt resource value: 0x7f050007
-			public const int textView1 = 2131034119;
+			// aapt resource value: 0x7f050006
+			public const int relativeLayout5 = 2131034118;
+			
+			// aapt resource value: 0x7f05000b
+			public const int space2 = 2131034123;
+			
+			// aapt resource value: 0x7f05000c
+			public const int textView1 = 2131034124;
 			
 			// aapt resource value: 0x7f050003
 			public const int topDecreaseButton = 2131034115;
